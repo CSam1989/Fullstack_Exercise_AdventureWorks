c89cfb59be332e28ad2fd8ed4cbbe9db44efa15a
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-10-21 13:52:30 +0200

    Get Customers query

---
+++ Application/Customers/Queries/Get/GetCustomerQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Customers.Queries.Get
+{
+    public class GetCustomerQuery: IRequest<CustomerVM>
+    {
+        public class GetCustomerQueryHandler : IRequestHandler<GetCustomerQuery, CustomerVM>
+        {
+            private readonly IAppDbContext _context;
+            private readonly IMapper _mapper;
+
+            public GetCustomerQueryHandler(IAppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<CustomerVM> Handle(GetCustomerQuery request, CancellationToken cancellationToken)
+            {
+                return new CustomerVM
+                {
+                    List = await _context.Customer
+                        .Include(c => c.Person)
+                        .Include(c => c.SalesOrderHeader)
+                        .Where(c => !string.IsNullOrEmpty(c.Person.FirstName) && !string.IsNullOrEmpty(c.Person.LastName))
+                        .ProjectTo<CustomerDto>(_mapper.ConfigurationProvider)
+                        .ToListAsync(cancellationToken)
+                };
+            }
+        }
+    }
+}
---
+++ Presentation/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Customers.Queries.Get;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<CustomerVM>> Get()
+        {
+            return await Mediator.Send(new GetCustomerQuery());
+        }
+    }
+}
