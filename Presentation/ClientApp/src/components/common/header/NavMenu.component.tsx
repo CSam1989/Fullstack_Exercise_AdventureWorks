820c02a43d329dbe3360bf4b21e1cef73d01bc68
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-10-23 13:20:27 +0200

    styling stuff

--- Presentation/ClientApp/src/components/common/header/NavMenu.component.tsx
+++ Presentation/ClientApp/src/components/common/header/NavMenu.component.tsx
@@ -1,6 +1,12 @@
 import "./NavMenu.styles.scss";
 
-import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import * as React from "react";
 import { Link as RouterLink, NavLink as RouterNavLink } from "react-router-dom";
 import AuthMenu from "./AuthMenu.component";
@@ -10,19 +16,21 @@ import { ApplicationState } from "../../../redux/types/State";
 export default () => {
   const { isLoggedIn } = useSelector((state: ApplicationState) => state.auth);
   return (
-    <AppBar position="sticky" color="primary">
-      <Toolbar>
-        <Button component={RouterLink} to="/" color="inherit">
-          <Typography variant="h6">AdventureWorks</Typography>
-        </Button>
-        <div className="space-between"></div>
-        {isLoggedIn ? (
-          <Button component={RouterNavLink} to="/customer" color="inherit">
-            Customers
+    <AppBar position="sticky" color="default">
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Button component={RouterLink} to="/" color="inherit">
+            <Typography variant="h6">AdventureWorks</Typography>
           </Button>
-        ) : null}
-        <AuthMenu />
-      </Toolbar>
+          <div className="space-between"></div>
+          {isLoggedIn ? (
+            <Button component={RouterNavLink} to="/customer" color="inherit">
+              Customers
+            </Button>
+          ) : null}
+          <AuthMenu />
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
