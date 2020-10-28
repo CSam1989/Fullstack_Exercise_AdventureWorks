f4c6652aac21363cd5279001c84739efa6940691
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-10-28 08:38:27 +0100

    clear redux store when login out

--- Presentation/ClientApp/src/redux/reducers/Root.reducer.ts
+++ Presentation/ClientApp/src/redux/reducers/Root.reducer.ts
@@ -1,14 +1,25 @@
+import { AuthActions } from "./../types/auth.types";
+import { ApiStatusActions } from "./../types/apiStatus.types";
+import { ApplicationState } from "./../types/State";
+import { LOGOUT } from "./../types/action.types";
 import { customerReducer } from "./Customer.reducer";
 import { authReducer } from "./Auth.reducer";
 import { combineReducers } from "redux";
 import apiStatusReducers from "./ApiStatus.reducer";
+import InitialState from "./initialState";
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   apiCallsInProgress: apiStatusReducers,
   auth: authReducer,
   data: customerReducer,
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof appReducer>;
+
+const rootReducer = (state: ApplicationState | undefined, action: any) => {
+  if (action.type === LOGOUT) state = undefined;
+
+  return appReducer(state, action);
+};
 
 export default rootReducer;
